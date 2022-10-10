c22c4bd3c3d6059e9f062f60970fc89b238a3e56
prathibha <prsthibha@gmail.com>
2022-10-10 09:15:49 +0530

    Implement routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router ,Route, Routes } from 'react-router-dom'
 import { Footer, Header } from "./components"
-import { Home } from './pages'
+import { Home, Product } from './pages'
 
 function App() {
 
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Home/>
+          <Routes>
+            <Route path='/' element={<Home />} exact />
+            <Route path='/product/:id' element={<Product />} exact />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
