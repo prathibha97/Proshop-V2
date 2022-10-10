426e58010af97a8618cd28db5842b854ab51415f
prathibha <prsthibha@gmail.com>
2022-10-10 08:56:50 +0530

    Add Rating component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,14 +5,14 @@ import products from "../products"
 const Home = () => {
   return (
     <>
-    <h1>Latest Products</h1>
-    <Row>
-        {products.map((product)=>(
-            <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} key={product._id}/>
-            </Col>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
         ))}
-    </Row>
+      </Row>
     </>
   )
 }
