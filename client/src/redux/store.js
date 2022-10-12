0decbfbf1e2f0ba93abaada166031c779615287e
Pasan Ratnayake <pasanratnayake@gmail.com>
2022-10-12 13:38:57 +0530

    legacy store config

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { productListReducer, productDetailsReducer } from './reducers/productReducer'
+import { legacy_createStore, combineReducers, applyMiddleware } from 'redux'
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
-export default configureStore({
-  reducer: {
-    productList: productListReducer,
-    productDetails: productDetailsReducer,
-  },
+import { productListReducer, productDetailsReducer } from './reducers/productReducer'
+import { cartReducer } from './reducers/cartReducers'
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage }
+}
+
+const reducer = combineReducers({
+
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer
 });
+
+const middleware = [thunk]
+
+const store = legacy_createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
+
