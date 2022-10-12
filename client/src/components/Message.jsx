6fdadcf65a3b9cea4f35b034f90dbbf54bd30479
Pasan Ratnayake <pasanratnayake@gmail.com>
2022-10-12 19:28:30 +0530

    Bug fix - typo

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -2,7 +2,7 @@ import {Alert} from 'react-bootstrap'
 
 const Message = ({ variant, children }) => {
     return (
-        <Alert varient={variant}>{children}</Alert>
+        <Alert variant={variant}>{children}</Alert>
     )
 }
 
