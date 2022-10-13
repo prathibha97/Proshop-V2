704f20e8f099e72c2f5f95d2c2d585cc5a3860ec
Pasan Ratnayake <pasanratnayake@gmail.com>
2022-10-13 10:55:20 +0530

    Add Custom Authentication Middleware

--- server/src/routes/users/user.route.js
+++ server/src/routes/users/user.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const {  authUser} = require('./user.controller')
+const { authUser, getUserProfile } = require('./user.controller')
+const protect = require('../../middleware/authMiddleware')
 
 const userRouter = express.Router();
 
 
 userRouter.post('/login', authUser);
+userRouter.route('/profile').get(protect, getUserProfile)
 
 
 module.exports = { userRouter };
