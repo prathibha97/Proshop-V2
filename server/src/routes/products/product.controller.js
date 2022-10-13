e2850ef3492ae7f76bcdd0eba91801e29bb6b19d
Pasan Ratnayake <pasanratnayake@gmail.com>
2022-10-13 09:07:31 +0530

    Clean Up By Using Controllers

---
+++ server/src/routes/products/product.controller.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const Product = require('../../models/Product')
+
+// @desc   Fetch all products
+// @route  Get /v1/api/products
+// @access Public
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products);
+})
+
+// @desc   Fetch a single product
+// @route  Get /v1/api/products/:id
+// @access Public
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+        res.status(200).json(product);
+    }
+    else {
+        res.status(404);
+        throw new Error('Product not found');
+    }
+})
+
+module.exports = { getProducts, getProductById }
--- server/src/routes/products/products.router.js
+++ server/src/routes/products/products.router.js
@@ -1,31 +1,11 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler');
-const Product = require('../../models/Product')
+const { getProducts, getProductById } = require('./product.controller')
 
 const productsRouter = express.Router();
 
-// @desc   Fetch all products
-// @route  Get /v1/api/products
-// @access Public
 
-productsRouter.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products);
-}));
+productsRouter.get('/', getProducts);
 
-// @desc   Fetch a single product
-// @route  Get /v1/api/products/:id
-// @access Public
-
-productsRouter.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-        res.status(200).json(product);
-    }
-    else {
-        res.status(404);
-        throw new Error('Product not found');
-    }
-}));
+productsRouter.get('/:id', getProductById);
 
 module.exports = { productsRouter };
