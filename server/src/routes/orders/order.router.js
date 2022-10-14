624efc6e5b0180b71b986f376b37b3b37308bfbd
Prathibha Ratnayake <prsthibha@gmail.com>
2022-10-14 21:45:20 +0530

    Bug fix

--- server/src/routes/orders/order.router.js
+++ server/src/routes/orders/order.router.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const { addOrderItems } = require('./order.controller')
-const { protect } = require('../../middleware/authMiddleware')
+const protect  = require('../../middleware/authMiddleware')
 
 const orderRouter = express.Router();
 
-
 orderRouter.route('/').post(protect, addOrderItems);
 
-module.exports = orderRouter;
+module.exports = {orderRouter};
