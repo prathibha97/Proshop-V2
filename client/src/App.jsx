90dae56fdae80c42ea907e68290dae682e0db90b
Pasan Ratnayake <pasanratnayake@gmail.com>
2022-10-14 19:06:58 +0530

    Add Shipping Screen & Save Address

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Footer, Header } from "./components"
-import { Cart, Home, Login, Product, Register, Profile } from './pages'
+import { Cart, Home, Login, Product, Register, Profile, Shipping } from './pages'
 
 function App() {
 
@@ -12,6 +12,7 @@ function App() {
         <Container>
           <Routes>
             <Route path='/login' element={<Login />} />
+            <Route path='/shipping' element={<Shipping />} />
             <Route path='/register' element={<Register />} />
             <Route path='/profile' element={<Profile />} />
             <Route path='/product/:id' element={<Product />} exact />
