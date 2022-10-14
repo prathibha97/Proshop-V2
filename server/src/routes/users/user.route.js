17e7eef121371758899f31295cfb4ef126301918
Pasan Ratnayake <pasanratnayake@gmail.com>
2022-10-14 10:33:32 +0530

    Add Update Profile Endpoint

--- server/src/routes/users/user.route.js
+++ server/src/routes/users/user.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { authUser, getUserProfile, registerUser } = require('./user.controller')
+const { authUser, getUserProfile, registerUser, UpdateUserProfile } = require('./user.controller')
 const protect = require('../../middleware/authMiddleware')
 
 const userRouter = express.Router();
 
 userRouter.route('/').post(registerUser)
 userRouter.post('/login', authUser);
-userRouter.route('/profile').get(protect, getUserProfile)
+userRouter.route('/profile').get(protect, getUserProfile).put(protect, UpdateUserProfile)
 
 
 module.exports = { userRouter };
