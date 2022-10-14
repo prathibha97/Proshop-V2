4b51e4ac54ed00170483b3452cf86cbbc06d2b09
Prathibha Ratnayake <prsthibha@gmail.com>
2022-10-14 20:11:11 +0530

    Bug fix

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -65,7 +65,7 @@ const Cart = () => {
         <Card>
           <ListGroup variant='flush'>
             <ListGroup.Item>
-              <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
+              <h2>Subtotal ({cartItems.reduce((acc, item) => acc + Number(item.qty), 0)}) items</h2>
               ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
             </ListGroup.Item>
             <ListGroup.Item>
