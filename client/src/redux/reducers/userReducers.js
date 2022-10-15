82f99a9858dd61a35dc4298e23bac3fe87939726
Prathibha Ratnayake <prsthibha@gmail.com>
2022-10-15 13:27:27 +0530

    Reset User Details & Orders When Logout

--- client/src/redux/reducers/userReducers.js
+++ client/src/redux/reducers/userReducers.js
@@ -1,6 +1,7 @@
 import {
     USER_DETAILS_FAIL,
     USER_DETAILS_REQUEST,
+    USER_DETAILS_RESET,
     USER_DETAILS_SUCCESS,
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
@@ -78,6 +79,10 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
                 loading: false,
                 error: payload,
             };
+            case USER_DETAILS_RESET:
+                return {
+                    user: {},
+                };
         default:
             return state;
     }
