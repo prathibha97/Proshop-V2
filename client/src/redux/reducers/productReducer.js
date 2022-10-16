4fffe214205eb5c13bb7183ac9dd5d32e91f4ac0
Prathibha Ratnayake <prsthibha@gmail.com>
2022-10-16 10:41:38 +0530

    Bug Fix

--- client/src/redux/reducers/productReducer.js
+++ client/src/redux/reducers/productReducer.js
@@ -2,6 +2,10 @@ import {
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_REQUEST,
   PRODUCT_CREATE_RESET,
+  PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_RESET,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_DELETE_FAIL,
   PRODUCT_DELETE_REQUEST,
@@ -46,7 +50,7 @@ export const productListReducer = (state = { products: [] }, action) => {
   }
 };
 
-export const productDetailsReducer = (state = { product: { review: [] } }, action) => {
+export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
   const { type, payload } = action;
   switch (type) {
     case PRODUCT_DETAILS_REQUEST:
@@ -137,6 +141,30 @@ export const productUpdateReducer = (state = { product: {} }, action) => {
       return {
         product: {}
       };
+    default:
+      return state;
+  }
+};
+
+export const productReviewCreateReducer = (state = { }, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case PRODUCT_CREATE_REVIEW_REQUEST:
+      return {
+        loading: true,
+      };
+    case PRODUCT_CREATE_REVIEW_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      };
+    case PRODUCT_CREATE_REVIEW_FAIL:
+      return {
+        loading: false,
+        error: payload,
+      };
+    case PRODUCT_CREATE_REVIEW_RESET:
+      return {};
     default:
       return state;
   }
