a2294a9d9cff609b3870785734fb6323c1ae7fcf
Prathibha Ratnayake <prsthibha@gmail.com>
2022-10-16 19:26:58 +0530

    Deployment Preperation Bug Fix

--- server/src/app.js
+++ server/src/app.js
@@ -8,29 +8,25 @@ const api = require('./routes/api')
 
 const app = express()
 
-if(process.env.NODE_ENV !== 'production'){
-    app.use(morgan('dev'))
-}else{
-app.use(express.static(path.join(__dirname, "..", "client", "build")))
-
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "client", "build", 'index.html'));
-  });
-}
-
-app.use(express.json())
-
 app.use(cors({
     origin: "http://localhost:5173",
 }))
+
+app.use(express.json())
+
+if(process.env.NODE_ENV == 'production'){
+    app.use(morgan('dev'))
+    app.use(express.static(path.join(__dirname, "..", "..", "client", "build")))
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "..", "..", "client", "build", 'index.html'));
+    });
+}
+
+app.use('/uploads', express.static(path.join(__dirname, 'src', 'routes', 'uploads')))
 
 app.use('/api/v1', api)
 
 app.use(notFound);
 app.use(errorHandler)
-
-
-app.use('/uploads', express.static(path.join(__dirname, 'src', 'routes', 'uploads')))
-
 
 module.exports = app
