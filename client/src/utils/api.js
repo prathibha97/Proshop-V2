821357b4981a3b8c9191a4956e05a35e3aca1796
Prathibha Ratnayake <prsthibha@gmail.com>
2022-11-16 13:22:29 +0530

    commit

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: 'v1',
+  baseURL: '/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
